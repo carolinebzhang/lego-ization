13f16a754356b336dad38a1cf34285e646aa204f
wiedmann-trey <trewiedmann@gmail.com>
2023-05-07 03:52:06 -0400

    fix

--- models.py
+++ models.py
@@ -83,7 +83,6 @@ class ResidualBlock(nn.Module):
 
     def forward(self, x):
         #print("residual forward")
-        x=x.float()
         return self.model(x) + x
 
 
@@ -98,15 +97,15 @@ class Generator(nn.Module):
         layers = []
 
         # input layer
-        layers.append(nn.Conv2d(in_channels=3, out_channels=conv_dim, kernel_size=7, stride=1, padding=3, bias=False, padding_mode='reflect', device=device))
-        layers.append(nn.InstanceNorm2d(conv_dim, affine=True, track_running_stats=True, device=device))
+        layers.append(nn.Conv2d(in_channels=3, out_channels=conv_dim, kernel_size=7, stride=1, padding=3, bias=True, padding_mode='reflect', device=device))
+        layers.append(nn.InstanceNorm2d(conv_dim, affine=False, track_running_stats=False, device=device))
         layers.append(nn.ReLU(inplace=True))
 
         # down sampling layers
         current_dims = conv_dim
         for i in range(2):
-            layers.append(nn.Conv2d(current_dims, current_dims*2, kernel_size=3, stride=2, padding=1, bias=False, device=device))
-            layers.append(nn.InstanceNorm2d(current_dims*2, affine=True, track_running_stats=True, device=device))
+            layers.append(nn.Conv2d(current_dims, current_dims*2, kernel_size=3, stride=2, padding=1, bias=True, device=device))
+            layers.append(nn.InstanceNorm2d(current_dims*2, affine=False, track_running_stats=False, device=device))
             layers.append(nn.ReLU(inplace=True))
             current_dims *= 2
 
@@ -116,19 +115,18 @@ class Generator(nn.Module):
 
         # up sampling layers
         for i in range(2):
-            layers.append(nn.ConvTranspose2d(current_dims, current_dims//2, kernel_size=3, stride=2, padding=1, output_padding=1, bias=False, device=device))
-            layers.append(nn.InstanceNorm2d(current_dims//2, affine=True, track_running_stats=True, device=device))
+            layers.append(nn.ConvTranspose2d(current_dims, current_dims//2, kernel_size=3, stride=2, padding=1, output_padding=1, bias=True, device=device))
+            layers.append(nn.InstanceNorm2d(current_dims//2, affine=False, track_running_stats=False, device=device))
             layers.append(nn.ReLU(inplace=True))
             current_dims = current_dims//2
 
         # output layer
-        layers.append(nn.Conv2d(current_dims, 3, kernel_size=7, stride=1, padding=3, bias=False, padding_mode='reflect', device=device))
+        layers.append(nn.Conv2d(current_dims, 3, kernel_size=7, stride=1, padding=3, bias=True, padding_mode='reflect', device=device))
         layers.append(nn.Tanh())
 
         self.model = nn.Sequential(*layers)
 
     def forward(self, x,):
-        x=x.float()
         #("generator forward")
         return self.model(x)
 
@@ -161,11 +159,10 @@ class Discriminator(nn.Module):
         self.model = nn.Sequential(*layers)
 
         # output layer
-        self.conv_src = nn.Conv2d(current_dim, 1, kernel_size=4, stride=1, padding=1, bias=False, device=device)
+        self.conv_src = nn.Conv2d(current_dim, 1, kernel_size=4, stride=1, padding=1, bias=True, device=device)
 
     def forward(self, x):
         #print("discriminator forward")
-        x=x.float()
         x = self.model(x)
         out_src = self.conv_src(x)
         return out_src
