222b84ef00a36bd80aa1ac267b894ec09a15072e
wiedmann-trey <trewiedmann@gmail.com>
2023-05-08 03:23:15 -0400

    training

--- generate.py
+++ generate.py
@@ -57,5 +57,5 @@ def generate_image(input_img_path, genre, model_path, output_image_path):
     data.save(final_path)
 
 if __name__=="__main__":
-    generate_image('ROI.png', genre='real_world', model_path='model62.pth', output_image_path="bigmode4house.jpg")
+    generate_image('APPLES.jpg', genre='real_world', model_path='bigmodel22ep.pth', output_image_path="bigmodel22ep_tolego")
     #generate_image('ORIGINAL.jpg', genre='lego', model_path='bad_discriminator.pth', output_image_path="bad_discriminator.jpg")
--- train.py
+++ train.py
@@ -3,14 +3,14 @@ import torch
 from datasets import get_data
 
 images_train_loader, images_test_loader = get_data(8)
-def train(epochs=50, save=True, load=False, model_path='model30ep.pth'):
+def train(epochs=50, save=True, load=True, model_path='bigmodel22.pth'):
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     model = CycleGAN()
     if load:
         model.load_state_dict(torch.load(model_path))
     model = model.to(device)
     path = "bigmodel"
-    path_num = 0
+    path_num = 23
     path_ending = ".pth"
     # paper uses lr=.0002, batch size=1, 100 epochs with lr and then 100 more with decaying lr
     # 128x128 or 256x256 images
